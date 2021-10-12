7b9cc2e1cca43ed6f38fde905d41020c7307d9a4
Leandro2585 <leo.real2585@gmail.com>
2021-10-12 11:42:32 -0300

    chore: add GraphQL

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,12 +1,23 @@
 import { ApolloServer, gql } from 'apollo-server'
 import '@main/config/module-alias'
+import { makeLoadStudentsControllerFactory } from '@main/factories/controllers'
 
 const typeDefs = gql`
+  type Student {
+    id: ID!
+    name: String!
+    email: String!
+    cpf: String!
+  }
   type Query {
-    
+    students: [Student!]!
   }
 `
-const resolvers = {}
+const resolvers = {
+  Query: {
+    students: () => makeLoadStudentsControllerFactory().handle
+  }
+}
 
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => console.log(`Server started at ${url}`))
